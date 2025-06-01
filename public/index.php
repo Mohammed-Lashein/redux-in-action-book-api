0f3ba5829b664593bd55281e268bb72eadbb3fab
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-01 11:11:44 +0300

    feat: add some code to learn more about preg_match

--- public/index.php
+++ public/index.php
@@ -11,6 +11,42 @@ $routeInfo = $dispatcher->dispatch(
     $_SERVER['REQUEST_URI']
 );
 
+dump("hello from idx");
+dump('hello world');
+$matches = [];
+/* 
+    Explanation of metacharacters : 
+    . => Match any singular character 
+        eg) .at matches 'bat' and 'rat'
+    * => 
+    - match zero or more of a character
+    - If you used the * by itself, it will match nothing 
+
+    Fancy combination (used in the provided .htaccess file) : 
+    .* 
+    => - Match zero or more occurrences of *any* character
+    - Will match the whole string 
+
+
+    That's what was causing a problem : 
+*/
+// preg_match('/.at/', 'example.com/public',$matches);
+// preg_match('/.*/', 'example.com/public',$matches);
+// preg_match('/graphql$/', 'example.com/graphql',$matches);
+// preg_match('/^.*/', 'example.com/graphql',$matches);
+
+// dump($matches);
+
+// My first fetch ever in php !
+// $data = file_get_contents('https://odyssey-lift-off-rest-api.herokuapp.com/tracks');
+// dump(json_decode($data));
+
+
+// learning regex start
+
+
+// learning regex end
+
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         // ... 404 Not Found
