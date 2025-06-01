510920e2c32ff24206d27715f3c313a316c3bc1c
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-01 19:20:24 +0300

    feat: add Database class

---
+++ core/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+  static $connection;
+  // The Database constructor should be protected to create a singleton and use it through our app using
+  // something like Laravel's service containers . But let's keep things simple for now . 
+  public function __construct($pdo = null)
+  {
+    try {
+      static::$connection = $pdo ?? new PDO(
+        'mysql:host=127.0.0.1;dbname=redux_in_action_book_api',
+        'root',
+        '',
+        [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+      );
+    } catch (PDOException $e) {
+      echo $e->getMessage();
+      // commented out so tests pass without actual connection to pdo
+      // die("failed connecting to db");
+    }
+  }
+
+  public function get(string $columns, string $table) {
+    $stmt = static::$connection->query("SELECT $columns FROM $table");
+    return $stmt->fetchAll();
+  }
+}
