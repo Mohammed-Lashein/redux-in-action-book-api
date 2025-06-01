85ec5c775f668d30227e340a375480eae8b896af
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-01 14:54:23 +0300

    feat: add the boilerplate required for the endpoint to work

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// For testing only, never in production !
+ini_set('display_errors', 1);
+
+use App\Controller\TaskController;
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/redux-in-action-book-api/graphql') {
+  /* 
+    Since I am using xampp, I wrote the request uri to be like so . 
+    If you are using php built in server, then you would need to test against '/graphql'
+  */
+  TaskController::handle();
+}
+
+
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace App\Controller;
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
+use GraphQL\GraphQL;
+
+class Task {
+  public static function all() {
+    return [
+      [
+        'id' => 1,
+        'title' => 'hello world !',
+        'description' => 'amz task',
+        'status' => 'In progress'
+      ],
+      [
+        'id' => 2,
+        'title' => 'hello world 2!',
+        'description' => 'amz task 2',
+        'status' => 'In progress --not started'
+      ]
+    ];
+  }
+}
+
+class TaskType extends ObjectType {
+  public function __construct() {
+    $config = [
+      'fields' => [
+        'id' => Type::id(),
+        'title' => Type::string(),
+        'description' => Type::string(),
+        'status' => Type::string(),
+      ]
+    ];
+    parent::__construct($config);
+  }
+}
+
+class TaskController {
+  public static function handle() {
+    $queryType = new ObjectType([
+      'name' => 'Query',
+      'fields' => [
+        'tasks' => [
+          'type' => Type::listOf(new TaskType()),
+          'resolve' => fn() => Task::all()
+        ]
+      ]
+    ]);
+
+    $schema = new Schema([
+      'query' => $queryType
+    ]);
+    $requestBody = file_get_contents('php://input'); // Raw JSON string from HTTP request
+    $parsedBody = json_decode($requestBody, true, 10); // Associative array decoded from JSON
+    $queryString = $parsedBody['query']; // The actual GraphQL query string
+
+    try {
+      $result = GraphQL::executeQuery($schema, $queryString, null, null, null);
+    } catch (\Throwable $e) {
+      //throw $th;
+      // no need to manually add 'error' to the result as graphql-php automatically returns errors 
+      // when we encounter them . 
+      // $result = [
+      //   'error' => $e->getMessage()
+      // ];
+      echo $e->getMessage();
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($result);
+  }
+}
