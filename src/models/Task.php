f3c52ec44e3c4db9e6d91c774145892fd3616e66
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-01 19:21:05 +0300

    refactor: make Task class extend the Model base class

--- src/models/Task.php
+++ src/models/Task.php
@@ -2,21 +2,8 @@
 
 namespace Src\Models;
 
-class Task {
-  public static function all() {
-    return [
-      [
-        'id' => 1,
-        'title' => 'hello world !',
-        'description' => 'amz task',
-        'status' => 'In progress'
-      ],
-      [
-        'id' => 2,
-        'title' => 'hello world 2!',
-        'description' => 'amz task 2',
-        'status' => 'In progress --not started'
-      ]
-    ];
-  }
+use Core\Model;
+
+class Task extends Model{
+  protected string $table = 'tasks';
 }
