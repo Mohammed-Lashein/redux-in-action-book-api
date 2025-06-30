00485c204c226e5d0067520950ad4215339b28d4
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-30 14:43:45 +0300

    fix: add the condition to respond correctly to the browser's preflight requests

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Headers: content-type");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header("Content-Type: application/json");
 
 
@@ -11,6 +12,12 @@ header("Content-Type: application/json");
 ini_set('display_errors', 1);
 
 use Src\Controller\TaskController;
+
+// To return a successful response for the browser when it sends the preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/redux-in-action-book-api/graphql') {
   /* 
