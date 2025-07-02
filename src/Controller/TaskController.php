5fad892b64d4a0e544d8955ac468b11823bdfef9
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-02 15:41:59 +0300

    feat: add mutation type to the TaskController schema

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,23 +3,30 @@
 
 namespace Src\Controller;
 
+use GraphQL\Error\DebugFlag;
 use GraphQL\Type\Schema;
 use GraphQL\GraphQL;
+use Src\Types\MutationType;
 use Src\Types\QueryType;
 
 class TaskController {
   public static function handle() {
     $queryType = QueryType::generateQueryType();
+    $mutationType = new MutationType();
 
     $schema = new Schema([
-      'query' => $queryType
+      'query' => $queryType,
+      'mutation' => $mutationType
     ]);
     $requestBody = file_get_contents('php://input'); // Raw JSON string from HTTP request
     $parsedBody = json_decode($requestBody, true, 10); // Associative array decoded from JSON
     $queryString = $parsedBody['query']; // The actual GraphQL query string
 
+    $variables = $parsedBody['variables'];
+
     try {
-      $result = GraphQL::executeQuery($schema, $queryString, null, null, null);
+      $result = GraphQL::executeQuery($schema, $queryString, null, null, $variables);
+      $result = $result->toArray(DebugFlag::INCLUDE_DEBUG_MESSAGE | DebugFlag::INCLUDE_TRACE);
     } catch (\Throwable $e) {
       //throw $th;
       // no need to manually add 'error' to the result as graphql-php automatically returns errors 
